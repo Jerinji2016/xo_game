00f8789f91e70ed43dd09af48818d4362ee15734
Jerin <jerinji2016@gmail.com>
2024-09-08 05:01:05 +0530

    added score board

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 
 import '../config/asset_config.dart';
 import 'widgets/play_area.dart';
+import 'widgets/score_board.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -15,38 +16,19 @@ class App extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Column(
             children: [
-              SizedBox(
-                height: 120,
+              Expanded(
                 child: Center(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 12),
-                        child: SvgPicture.asset(
-                          Assets.logo,
-                          height: 48,
-                          colorFilter: const ColorFilter.mode(
-                            Color(0xFFF1F1F1),
-                            BlendMode.srcIn,
-                          ),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 12),
-                        child: Text(
-                          'XO GAME',
-                          style: TextStyle(
-                            fontSize: 48,
-                            color: Color(0xFFF1F1F1),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  child: _buildLogo(),
                 ),
               ),
-              Expanded(
+              const Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12),
+                  child: ScoreBoard(),
+                ),
+              ),
+              const Expanded(
+                flex: 5,
                 child: Center(
                   child: PlayArea(),
                 ),
@@ -55,6 +37,35 @@ class App extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 12),
+          child: SvgPicture.asset(
+            Assets.logo,
+            height: 48,
+            colorFilter: const ColorFilter.mode(
+              Color(0xFFF1F1F1),
+              BlendMode.srcIn,
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12),
+          child: Text(
+            'XO GAME',
+            style: TextStyle(
+              fontSize: 48,
+              color: Color(0xFFF1F1F1),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
