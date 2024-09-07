0f30d1cf0b9d40c7e9ae53044b61ef1e0c01d666
Jerin <jerinji2016@gmail.com>
2024-09-08 05:00:45 +0530

    added score board

---
+++ lib/ui/widgets/score_board.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ScoreBoard extends StatefulWidget {
+  const ScoreBoard({super.key});
+
+  @override
+  State<ScoreBoard> createState() => _ScoreBoardState();
+}
+
+class _ScoreBoardState extends State<ScoreBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+        child: FittedBox(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildPlayerScore('Player X:', '10'),
+              _buildPlayerScore('Player O:', '6'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlayerScore(String label, String score) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(color: Colors.white),
+        ),
+        const SizedBox(width: 12),
+        Text(
+          score,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ],
+    );
+  }
+}
