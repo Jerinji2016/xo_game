46ae2533d8ac07137958abec9816a10bd01995c2
Jerin <jerinji2016@gmail.com>
2024-09-08 10:45:42 +0530

    game page transitions complete

--- lib/ui/pages/game_page.dart
+++ lib/ui/pages/game_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 
 import '../../game_provider/game_provider.dart';
+import '../transition_mixin/fade_transition.mixin.dart';
 import '../widgets/logo.dart';
 import '../widgets/play_area.dart';
 import '../widgets/primary_button.dart';
@@ -14,53 +16,93 @@ class GamePage extends StatefulWidget {
   State<GamePage> createState() => _GamePageState();
 }
 
-class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
-  late final _animationController = AnimationController(
+class _GamePageState extends State<GamePage> with TickerProviderStateMixin, FadeTransitionMixin {
+  late final _entryController = AnimationController(
     vsync: this,
     duration: const Duration(seconds: 1),
-    reverseDuration: const Duration(milliseconds: 300),
+  );
+
+  late final _exitController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 1),
   );
 
   @override
   void initState() {
     super.initState();
     Provider.of<GameProvider>(context, listen: false).reset();
+    SchedulerBinding.instance.addPostFrameCallback(
+      (timestamp) => _entryController.forward(),
+    );
   }
 
   void _onBackTapped() {
-    Provider.of<GameProvider>(context, listen: false).returnToMenu();
+    if (_exitController.isAnimating) return;
+
+    _exitController.reverse().then(
+          (value) => Navigator.of(context).pop(),
+        );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Expanded(
-            child: Center(
-              child: Logo(),
-            ),
-          ),
-          const Expanded(
-            child: Center(
-              child: ScoreBoard(),
-            ),
-          ),
-          const Expanded(
-            flex: 3,
-            child: Center(
-              child: PlayArea(),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24),
-            child: PrimaryButton(
-              text: 'Go Back',
-              onTap: _onBackTapped,
-            ),
-          ),
-        ],
+      body: AnimatedBuilder(
+        animation: _entryController,
+        builder: (context, child) {
+          return Column(
+            children: [
+              const Expanded(
+                child: Center(
+                  child: Logo(),
+                ),
+              ),
+              const Expanded(
+                child: Center(
+                  child: ScoreBoard(),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Center(
+                  child: fadeIn(
+                    controller: _entryController,
+                    end: 0.7,
+                    child: const PlayArea(),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                child: AnimatedBuilder(
+                  animation: _exitController,
+                  builder: (context, child) {
+                    return fadeOut(
+                      controller: _exitController,
+                      child: child!,
+                    );
+                  },
+                  child: fadeIn(
+                    controller: _entryController,
+                    begin: 0.3,
+                    child: PrimaryButton(
+                      text: 'Go Back',
+                      onTap: _onBackTapped,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _entryController.dispose();
+    _exitController.dispose();
   }
 }
