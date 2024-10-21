46e801193f5f2fadee200e9b8e58458b499dd96a
Jerin <jerinji2016@gmail.com>
2024-10-21 18:30:53 +0400

    added glassmorphism to play button

--- lib/ui/widgets/play_button.dart
+++ lib/ui/widgets/play_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 
 import '../../config/theme_config.dart';
 
@@ -14,65 +15,88 @@ class PlayButton extends StatefulWidget {
   State<PlayButton> createState() => _PlayButtonState();
 }
 
-class _PlayButtonState extends State<PlayButton> {
+class _PlayButtonState extends State<PlayButton> with TickerProviderStateMixin {
   bool _isHovered = false;
+
+  late final _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.white10,
-            blurRadius: 10,
-            offset: _isHovered ? const Offset(-5, 5) : const Offset(5, 5),
-          ),
-        ],
-        gradient: LinearGradient(
+    return Material(
+      color: Colors.transparent,
+      elevation: _isHovered ? 20 :10,
+      shadowColor: _isHovered ? Colors.grey.shade600 : Colors.grey.shade800,
+      borderRadius: BorderRadius.circular(16),
+      child: GlassmorphicContainer(
+        width: 280,
+        height: 90,
+        blur: 3,
+        border: 2,
+        borderRadius: 16,
+        linearGradient: LinearGradient(
           begin: _isHovered ? const Alignment(0.2, -1) : Alignment.topLeft,
           end: _isHovered ? Alignment.bottomCenter : Alignment.bottomRight,
           colors: const [
-            darkSecondary,
-            darkSecondary,
+            darkPrimary,
+            darkPrimary,
             accentColor,
           ],
         ),
-      ),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: widget.onTap,
-          onHover: (hover) => setState(() => _isHovered = hover),
-          borderRadius: BorderRadius.circular(16),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(vertical: 24, horizontal: 48),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 6),
-                  child: Icon(
-                    Icons.play_arrow,
-                    color: Colors.white,
-                    size: 48,
+        borderGradient: LinearGradient(
+          begin: _isHovered ? const Alignment(0.2, -1) : Alignment.topLeft,
+          end: _isHovered ? Alignment.bottomCenter : Alignment.bottomRight,
+          colors: const [
+            darkPrimary,
+            darkPrimary,
+            accentColor,
+          ],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: widget.onTap,
+            onHover: (hover) => setState(() => _isHovered = hover),
+            borderRadius: BorderRadius.circular(16),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 24, horizontal: 48),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 6),
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                      size: 48,
+                    ),
                   ),
-                ),
-                SizedBox(width: 12),
-                Text(
-                  'Play',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 48,
-                    height: 0,
+                  SizedBox(width: 12),
+                  Text(
+                    'Play',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 48,
+                      height: 0,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    if(_animationController.isAnimating) {
+      _animationController.stop();
+    }
+    _animationController.dispose();
   }
 }
