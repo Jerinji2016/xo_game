00f86592cf105d34a298c1b606b4f21ae32cd85b
Jerin <jerinji2016@gmail.com>
2024-10-21 18:48:02 +0400

    added glassmorphism to play button complete

--- lib/ui/widgets/play_button.dart
+++ lib/ui/widgets/play_button.dart
@@ -16,49 +16,70 @@ class PlayButton extends StatefulWidget {
 }
 
 class _PlayButtonState extends State<PlayButton> with TickerProviderStateMixin {
-  bool _isHovered = false;
-
   late final _animationController = AnimationController(
     vsync: this,
-    duration: const Duration(milliseconds: 200),
+    duration: const Duration(milliseconds: 300),
+    reverseDuration: const Duration(milliseconds: 100)
   );
+
+  void _onHover(bool isHovered) => isHovered ? _animationController.forward() : _animationController.reverse();
+
+  Animation<Alignment> _getAlignmentAnimation(Alignment begin, Alignment end) => AlignmentTween(
+        begin: begin,
+        end: end,
+      ).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.ease),
+      );
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
-      elevation: _isHovered ? 20 :10,
-      shadowColor: _isHovered ? Colors.grey.shade600 : Colors.grey.shade800,
-      borderRadius: BorderRadius.circular(16),
-      child: GlassmorphicContainer(
-        width: 280,
-        height: 90,
-        blur: 3,
-        border: 2,
-        borderRadius: 16,
-        linearGradient: LinearGradient(
-          begin: _isHovered ? const Alignment(0.2, -1) : Alignment.topLeft,
-          end: _isHovered ? Alignment.bottomCenter : Alignment.bottomRight,
-          colors: const [
-            darkPrimary,
-            darkPrimary,
-            accentColor,
-          ],
-        ),
-        borderGradient: LinearGradient(
-          begin: _isHovered ? const Alignment(0.2, -1) : Alignment.topLeft,
-          end: _isHovered ? Alignment.bottomCenter : Alignment.bottomRight,
-          colors: const [
-            darkPrimary,
-            darkPrimary,
-            accentColor,
-          ],
-        ),
+      child: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          final beginValue = _getAlignmentAnimation(
+            const Alignment(0.2, -1),
+            Alignment.topLeft,
+          ).value;
+
+          final endValue = _getAlignmentAnimation(
+            Alignment.bottomCenter,
+            Alignment.bottomRight,
+          ).value;
+
+          return GlassmorphicContainer(
+            width: 280,
+            height: 90,
+            blur: 3,
+            border: 2,
+            borderRadius: 16,
+            linearGradient: LinearGradient(
+              begin: beginValue,
+              end: endValue,
+              colors: [
+                darkPrimary.withOpacity(0.3),
+                accentColor.withOpacity(0.3),
+              ],
+            ),
+            borderGradient: LinearGradient(
+              begin: beginValue,
+              end: endValue,
+              colors: [
+                Colors.purple.withOpacity(0.6),
+                accentColor,
+              ],
+            ),
+            child: child,
+          );
+        },
         child: Material(
           color: Colors.transparent,
           child: InkWell(
             onTap: widget.onTap,
-            onHover: (hover) => setState(() => _isHovered = hover),
+            onHover: _onHover,
+            highlightColor: Colors.white.withOpacity(0.05),
+            splashColor: Colors.white10,
             borderRadius: BorderRadius.circular(16),
             child: const Padding(
               padding: EdgeInsets.symmetric(vertical: 24, horizontal: 48),
@@ -94,7 +115,7 @@ class _PlayButtonState extends State<PlayButton> with TickerProviderStateMixin {
   void dispose() {
     super.dispose();
 
-    if(_animationController.isAnimating) {
+    if (_animationController.isAnimating) {
       _animationController.stop();
     }
     _animationController.dispose();
